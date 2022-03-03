a733cf3835f1dfd955f8d599fc25228d6986f500
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-03-03 00:37:43 -0800

    created operations functions working in console

---
+++ script.js
@@ -0,0 +1,20 @@
+// function that adds two numbers
+function add(a,b) {
+    return a + b;
+}
+// function that substract two numbers
+function substract(a, b) {
+    return a - b;
+}
+// function that multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+// function that divide two numbers
+function divide(a,b) {
+    return a / b;
+}
+console.log(add(2,3));
+console.log(substract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3))
